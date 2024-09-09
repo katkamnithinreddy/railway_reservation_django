959ef6d3d6db83b03d8a1f8d3b6715ed3455da4d
katkamnithinreddy <114159603+katkamnithinreddy@users.noreply.github.com>
2024-09-10 00:59:06 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,17 +1,22 @@
+"""login URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path,include
-from . import views
+
 urlpatterns = [
-  path('',views.home,name="home"),
-  path('signup',views.signup,name="signup"),
-  path('signin',views.signin,name="signin"),
-  path('signout',views.signout,name="signout"),
-  path('activate/<uidb64>/<token>',views.activate,name="activate"),
-  path('search',views.search,name="search"),
-  path('show',views.show,name="show"),
-  path('add',views.add,name="add"),
-  path('booking',views.booking,name="booking"),
-  path('bookform/',views.bookform,name="booking"),
-  path('mybookings',views.mybookings,name="mybookings"),
-  path('makepayment',views.makepayment,name="makepayment")
+    path('admin/', admin.site.urls),
+    path('',include('authentication.urls')),
 ]
